e681b96fe5065aaf63f363730e60c99d2201e65a
박세종 <sejong.park.2000@gmail.com>
2024-07-13 15:45:57 +0900

    feat: 설명 페이지 스타일 수정

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -4,10 +4,17 @@ import styled from 'styled-components';
 const IntroContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
+  align-items: center; /* 중앙 정렬 */
+  justify-content: center; /* 중앙 정렬 */
   height: 80vh;
   padding: 20px;
+  background-color: #f5f5f7; /* 애플 웹사이트 배경 색상 */
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; /* 폰트 설정 */
+
+  @media (max-width: 768px) {
+    padding: 20px 10px; /* 모바일 패딩 조정 */
+    text-align: center; /* 모바일 텍스트 중앙 정렬 */
+  }
 `;
 
 interface IntroTextProps {
@@ -17,22 +24,22 @@ interface IntroTextProps {
 
 const IntroText = styled.div<IntroTextProps>`
   font-size: ${(props) => props.size || 'inherit'};
-  text-align: left;
+  text-align: center; /* 텍스트 중앙 정렬 */
   margin: 10px;
-  color: ${(props) => props.color || 'inherit'};
-  font-family: 'Arial', sans-serif;
+  color: ${(props) => props.color || '#333333'}; /* 기본 색상 설정 */
+  font-weight: ${(props) => (props.size === '36px' ? '600' : '400')}; /* 헤딩과 본문 폰트 웨이트 설정 */
 `;
 
 const AboutPage: React.FC = () => {
   return (
     <IntroContainer>
-      <IntroText size="36px" color="#C0C0C0">
+      <IntroText size="36px" color="#333333">
         About this service...
-      </IntroText>{' '}
-      <IntroText size="20px" color="#C0C0C0">
+      </IntroText>
+      <IntroText size="20px" color="#555555">
         이 서비스에 대하여
-      </IntroText>{' '}
-      <IntroText size="20px">
+      </IntroText>
+      <IntroText size="20px" color="#777777">
         이 서비스는 테커 팀루나 프로젝트팀 Achieve에서 만든 테커 동아리방 예약 시스템 입니다.
       </IntroText>
     </IntroContainer>
